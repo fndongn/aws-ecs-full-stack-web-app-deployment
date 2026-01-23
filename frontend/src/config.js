37167da99c1d26c0a4041e7486a1c5594b7fd237
Francisco Ndong <93534625+fndongn@users.noreply.github.com>
2026-01-22 22:03:47 -0600

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const config = {
-  backendUrl: "http://deployment-ecs-alb-315687768.us-east-1.elb.amazonaws.com/api",
+  backendUrl: "http://deployment-ecs-alb-315687768.us-east-1.elb.amazonaws.com/api/",
 };
 export default config;
